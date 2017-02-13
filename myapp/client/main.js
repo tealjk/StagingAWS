73451c42aef62fa19c1da864d1ae3282cde3d7c8
tealjk <tealjk914@gmail.com>
2017-02-13 06:12:46 -0500

    second commit

--- myapp/client/main.js
+++ myapp/client/main.js
@@ -3,7 +3,8 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 import '../imports/ui/body.js';
-
+import '../imports/ui/task.js';
+import '../imports/ui/task/html';
 
 
 
